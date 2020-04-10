68dca064de2e313bfd72ff599ee67f99590686bd
abrormukimov <abrormm34@gmail.com>
2020-04-10 15:31:36 +0500

    Add my_ach, my_each_with_index and my_select

--- script.rb
+++ script.rb
@@ -1,13 +1,26 @@
 module Enumerable
-
-  def my_each(a, b)
-
-    yield(a, b)
-
+  def my_each
+    i = 0
+    while i < self.size
+      yield(self[i])
+      i += 1
+    end
   end
-
-
+  def my_each_with_index
+    for j in 0...self.size
+      yield(self[j], j)
+    end
+  end
+  def my_select
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+  end
 end
 
-puts my_each(5, 6) { |a, b| a + b}
-
+include Enumerable
+[1, 2, 3, 4, 5].my_each { |x| p x }
+[1, 2, 3, 4, 5].my_each_with_index { |x, y| puts "#{x} with index #{y}" }
+[1, 2, 3, 4, 5].my_select { |x| puts x % 2 == 0 }
