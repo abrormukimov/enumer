2020c1dafa2540a9d12c721f5e76103c7799cae5
abrormukimov <abrormm34@gmail.com>
2020-04-10 15:43:34 +0500

    Add my_all

--- script.rb
+++ script.rb
@@ -18,9 +18,21 @@ module Enumerable
       i += 1
     end
   end
+  def my_all
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+  end
 end
 
 include Enumerable
 [1, 2, 3, 4, 5].my_each { |x| p x }
+puts "-----------"
 [1, 2, 3, 4, 5].my_each_with_index { |x, y| puts "#{x} with index #{y}" }
+puts "-----------"
 [1, 2, 3, 4, 5].my_select { |x| puts x % 2 == 0 }
+puts "-----------"
+[1, 2, 3, 4, 5].my_all { |x| puts x > 3 }
+puts "-----------"
