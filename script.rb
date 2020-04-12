b07099ea8a487bee233a0e7b4f2996dd88b9f905
abrormukimov <abrormm34@gmail.com>
2020-04-12 17:09:06 +0500

    Check for rubocop

--- script.rb
+++ script.rb
@@ -108,32 +108,3 @@ module Enumerable
     multi
   end
 end
-
-include Enumerable
-p [1, 2, 3, 4, 5].my_each
-puts "-----------"
-p [1, 2, 3, 4, 5].my_each_with_index
-puts "-----------"
-p [1, 2, 3, 4, 5].my_select
-puts
-puts "-----------"
-p ["ass", "ss", "fish"].my_all?(/g/)
-puts
-puts "-----------"
-print ["dish", "fish", 3].my_any?(Integer)
-puts
-puts "-----------"
-print ["dish", "dosh", "qosh"].my_none?(/c/)
-puts
-puts "-----------"
-print [1, 12, 13, 14, 3].my_count
-puts
-puts "-----------"
-p [1, 2, 3, 4, 6].my_map { |x| x < 6 }
-puts "-----------"
-p [2, 3, 4].my_inject { |accum, num| accum * num }
-puts
-puts "-----------"
-print [1, 2, 3, 5, 7].multiply_els { |accum, num| accum * num }
-puts
-puts "-----------"
